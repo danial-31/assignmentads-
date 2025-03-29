d71fae256af730c13f09d7ee29a95e4957b386c4
danialtapishev <daniar.t3009@gmail.com>
2025-03-29 13:30:11 +0500

    3

---
+++ src/MinRecursive.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MinRecursive {
+    static int findMin(int[] arr, int n) {
+        if (n == 1) return arr[0];
+        return Math.min(arr[n - 1], findMin(arr, n - 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findMin(arr, n));
+        sc.close();
+    }
+}
+
+
